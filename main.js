23bc9452b7376d4527c2edd5289de76f45ca6290
Your Name <96867921+UYS2@users.noreply.github.com>
2023-06-28 17:30:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 import * as THREE from 'three';
-import { Vector3 } from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+class Cube {
+  constructor(vertices, center) {
+    this.vertices = vertices;
+    this.center = center;
+  }
+
+  getFurthestPointInDirection(direction) {
+    let furthestPoint = null;
+    let maxDistance = -Infinity;
+
+    for (let i = 0; i < this.vertices.length; i++) {
+      const vertex = this.vertices[i].clone();
+      vertex.add(this.center);
+      const distance = vertex.dot(direction);
+
+      if (distance > maxDistance) {
+        maxDistance = distance;
+        furthestPoint = vertex;
+      }
+    }
+
+    return furthestPoint;
+  }
+}
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 5;
@@ -8,223 +32,147 @@ camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-let objectA, objectB;
-document.addEventListener('click', () => {
-  
 
-	
-  const loader = new GLTFLoader();
-  loader.load(
-	'scene.gltf',
-	function (gltf) {
-	  const objectA = gltf.scene.clone();
-	  scene.add(objectA);
-	});
-	loader.load(
-		'scene.gltf',
-		function (gltf) {
-		  const objectB = gltf.scene.clone();
-		  scene.add(objectB);
-		});
-  // Collider
-  class Collider {
-    FindFurthestPoint(direction) {}
+const amplitude = 2;
+const frequency = 0.005;
+
+const geometry1 = new THREE.BoxGeometry(1, 1, 1);
+const material1 = new THREE.MeshBasicMaterial({ color: 0xffffff }); // White color
+const cube1 = new THREE.Mesh(geometry1, material1);
+scene.add(cube1);
+cube1.position.set(1, 0, 0);
+const cube1Vertices = Array.from(geometry1.attributes.position.array).reduce((acc, _, i, array) => {
+  if (i % 3 === 0) {
+    const vertex = new THREE.Vector3(array[i], array[i + 1], array[i + 2]);
+    acc.push(vertex);
   }
+  return acc;
+}, []);
 
-  class MeshCollider extends Collider {
-    constructor(vertices) {
-      super();
-      this.vertices = vertices;
-    }
+const geometry2 = new THREE.BoxGeometry(1, 1, 1);
+const material2 = new THREE.MeshBasicMaterial({ color: 0xffffff });
+const cube2 = new THREE.Mesh(geometry2, material2);
+scene.add(cube2);
+cube2.position.set(-1, 0, 0);
+const cube2Vertices = Array.from(geometry2.attributes.position.array).reduce((acc, _, i, array) => {
+  if (i % 3 === 0) {
+    const vertex = new THREE.Vector3(array[i], array[i + 1], array[i + 2]);
+    acc.push(vertex);
+  }
+  return acc;
+}, []);
 
-    FindFurthestPoint(direction) {
-      let maxPoint;
-      let maxDistance = -Infinity;
+const gjk = (cube1, cube2) => {
+  const support = (cube1, cube2, direction) => {
+    const point1 = cube1.getFurthestPointInDirection(direction);
+    const point2 = cube2.getFurthestPointInDirection(direction.clone().negate());
+    return point1.clone().sub(point2);
+  };
 
-      for (let vertex of this.vertices) {
-        const distance = vertex.dot(direction);
-        if (distance > maxDistance) {
-          maxDistance = distance;
-          maxPoint = vertex;
-        }
+  const handleSimplex = (simplex, direction) => {
+    const a = simplex[simplex.length - 1];
+    const ao = a.negate();
+
+    if (simplex.length === 2) {
+      const b = simplex[0];
+      const ab = b.sub(a);
+      direction.copy(ab.cross(ao));
+
+      if (direction.isZero()) {
+        direction.set(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5).normalize();
       }
 
-      return maxPoint;
-    }
-  }
-
-  // Simplex
-  class Simplex {
-    constructor() {
-      this.m_points = [new Vector3(), new Vector3(), new Vector3(), new Vector3()];
-      this.m_size = 0;
+      return;
     }
 
-    [Symbol.iterator]() {
-      return this.m_points.slice(0, this.m_size)[Symbol.iterator]();
-    }
+    if (simplex.length === 3) {
+      const b = simplex[simplex.length - 2];
+      const c = simplex[simplex.length - 3];
+      const ab = b.sub(a);
+      const ac = c.sub(a);
+      direction.copy(ab.cross(ac));
 
-    push_front(point) {
-      this.m_points = [point, ...this.m_points.slice(0, 3)];
-      this.m_size = Math.min(this.m_size + 1, 4);
-    }
-
-    get(index) {
-      return this.m_points[index];
-    }
-
-    getSize() {
-      return this.m_size;
-    }
-
-    setPoints(...args) {
-      for (let i = 0; i < args.length; i++) {
-        this.m_points[i] = args[i];
-      }
-      this.m_size = args.length;
-    }
-  }
-  function Support(colliderA, colliderB, direction) {
-    const pointA = colliderA.FindFurthestPoint(direction.clone());
-    const pointB = colliderB.FindFurthestPoint(direction.clone().negate());
-    return pointA.sub(pointB);
-  }
-
-  function GJK(colliderA, colliderB) {
-    let support = Support(colliderA, colliderB, new Vector3(1, 0, 0));
-
-    const points = new Simplex();
-    points.push_front(support);
-
-    let direction = support.negate();
-
-    while (true) {
-      support = Support(colliderA, colliderB, direction);
-
-      if (support.dot(direction) <= 0) {
-        return false;
+      if (direction.isZero()) {
+        direction.copy(ac.cross(ab).cross(ac));
       }
 
-      points.push_front(support);
-      if (NextSimplex(points, direction)) {
-        return true;
+      if (direction.isZero()) {
+        direction.copy(ab.cross(ac.negate()).cross(ab));
       }
-    }
-  }
 
-  function NextSimplex(points, direction) {
-    switch (points.getSize()) {
-      case 2:
-        return Line(points, direction);
-      case 3:
-        return Triangle(points, direction);
-      case 4:
-        return Tetrahedron(points, direction);
+      return;
     }
 
-    return false;
-  }
+    const b = simplex[simplex.length - 2];
+    const c = simplex[simplex.length - 3];
+    const d = simplex[simplex.length - 4];
+    const ab = b.sub(a);
+    const ac = c.sub(a);
+    const ad = d.sub(a);
+    const abc = ab.cross(ac);
+    const acd = ac.cross(ad);
 
-  function SameDirection(direction, ao) {
-    return direction.dot(ao) > 0;
-  }
-
-  function Line(points, direction) {
-    const a = points.get(0);
-    const b = points.get(1);
-
-    const ab = b.clone().sub(a);
-    const ao = a.clone().negate();
-
-    if (SameDirection(ab, ao)) {
-      direction.copy(ab.clone().cross(ao).cross(ab));
-    } else {
-      points.setPoints(a);
-      direction.copy(ao);
+    if (abc.dot(ad.cross(ab)) > 0) {
+      simplex.splice(simplex.length - 3, 1);
+      direction.copy(ab.cross(ad).cross(ab));
+      return;
     }
 
-    return false;
-  }
-
-  function Triangle(points, direction) {
-    const a = points.get(0);
-    const b = points.get(1);
-    const c = points.get(2);
-
-    const ab = b.clone().sub(a);
-    const ac = c.clone().sub(a);
-    const ao = a.clone().negate();
-
-    const abc = ab.clone().cross(ac);
-
-    if (SameDirection(abc.cross(ac), ao)) {
-      if (SameDirection(ac, ao)) {
-        points.setPoints(a, c);
-        direction.copy(ac.clone().cross(ao).cross(ac));
-      } else {
-        return Line((points = new Simplex(a, b)), direction);
-      }
-    } else {
-      if (SameDirection(ab.cross(abc), ao)) {
-        return Line((points = new Simplex(a, b)), direction);
-      } else {
-        if (SameDirection(abc, ao)) {
-          direction.copy(abc);
-        } else {
-          points.setPoints(a, c, b);
-          direction.copy(abc.clone().negate());
-        }
-      }
+    if (acd.dot(ab.cross(ac)) > 0) {
+      simplex.splice(simplex.length - 2, 1);
+      direction.copy(ac.cross(ab).cross(ac));
+      return;
     }
 
-    return false;
-  }
-
-  function Tetrahedron(points, direction) {
-    const a = points.get(0);
-    const b = points.get(1);
-    const c = points.get(2);
-    const d = points.get(3);
-
-    const ab = b.clone().sub(a);
-    const ac = c.clone().sub(a);
-    const ad = d.clone().sub(a);
-    const ao = a.clone().negate();
-
-    const abc = ab.clone().cross(ac);
-    const acd = ac.clone().cross(ad);
-    const adb = ad.clone().cross(ab);
-
-    if (SameDirection(abc, ao)) {
-      return Triangle((points = new Simplex(a, b, c)), direction);
-    }
-
-    if (SameDirection(acd, ao)) {
-      return Triangle((points = new Simplex(a, c, d)), direction);
-    }
-
-    if (SameDirection(adb, ao)) {
-      return Triangle((points = new Simplex(a, d, b)), direction);
+    if (abc.dot(ac.cross(ad)) > 0) {
+      simplex.splice(simplex.length - 3, 2);
+      direction.copy(ad.cross(ac).cross(ad));
+      return;
     }
 
     return true;
-  }
-  
-  const audioContext = new AudioContext();
-  const beepAudio = new Audio();
-  beepAudio.src = 'beep.wav';
-  beepAudio.addEventListener('canplaythrough', () => {
-    if (GJK(objectA, objectB)) {
-      beepAudio.play();
-      material.color.set(0xff0000);
-    } else {
-      material.color.set(0xffffff);
-    }
-  });
-  function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
-  }
+  };
 
-  animate();
+  const simplex = [support(cube1, cube2, new THREE.Vector3(1, 1, 1))];
+  const direction = simplex[0].negate();
+
+  while (true) {
+    const supportPoint = support(cube1, cube2, direction);
+
+    if (supportPoint.dot(direction) <= 0) {
+      return false;
+    }
+
+    simplex.push(supportPoint);
+
+    const result = handleSimplex(simplex, direction);
+    if (result === true) {
+      return true;
+    }
+  }
+};
+
+const beepAudio = new Audio();
+beepAudio.src = 'beep.wav';
+beepAudio.addEventListener('canplaythrough', () => {
+  if (gjk(cube1, cube2)) {
+    beepAudio.play();
+    material1.color.set(0xff0000);
+    material2.color.set(0xff0000);
+  } else {
+    material1.color.set(0xffffff);
+    material2.color.set(0xffffff);
+  }
 });
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  const time = Date.now() * frequency;
+  const displacement1 = Math.sin(time) * amplitude;
+  const displacement2 = Math.sin(time + Math.PI) * amplitude;
+  cube1.position.x = displacement1;
+  cube2.position.x = displacement2;
+  renderer.render(scene, camera);
+};
+
+animate();
